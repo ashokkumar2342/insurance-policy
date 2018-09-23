0cd77340617e4833bb748a429b100a066233a837
ashokkumar2342 <ashokkumar2342@gmail.com>
2018-09-23 23:47:42 +0530

    agent change

--- app/Http/Controllers/Agent/Auth/LoginController.php
+++ app/Http/Controllers/Agent/Auth/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Student\Auth;
+namespace App\Http\Controllers\Agent\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
